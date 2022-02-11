d3b9119787d3ef2e3571510e5254f04041d1a41c
DávalosLeonardo <davaloslm@hotmail.com.ar>
2022-02-11 15:58:12 -0300

    Convierte componente Movie a stateful y agrega MovieList

--- front/src/components/Movie.js
+++ front/src/components/Movie.js
@@ -1,57 +1,83 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
 import MovieList from './MovieList';
 
-function Movie(){
-    return(
-        <React.Fragment>
-				    {/*<!-- PRODUCTS LIST -->*/}
-					<h1 className="h3 mb-2 text-gray-800">All the movies in the Database</h1>
-					
-					{/*<!-- DataTales Example -->*/}
-					<div className="card shadow mb-4">
-						<div className="card-body">
-							<div className="table-responsive">
-								<table className="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-									<thead>
-										<tr>
-                                            <th>Id</th>
-                                            <th>Titulo</th>
-                                            <th>Calificación</th>
-                                            <th>Premios</th>
-                                            <th>Duración</th>
-										</tr>
-									</thead>
-									<tfoot>
-										<tr>
-                                            <th>Id</th>
-                                            <th>Titulo</th>
-                                            <th>Calificación</th>
-                                            <th>Premios</th>
-                                            <th>Duración</th>
-										</tr>
-									</tfoot>
-									<tbody>
-										<tr>
-											<td>01</td>
-											<td>Reto al destino</td>
-											<td>20</td>
-                                            <td>15</td>
-											<td>120</td>
-										</tr>
-										<tr>
-											<td>02</td>
-											<td>La caida del halcon negro</td>
-											<td>10</td>
-											<td>18</td>
-											<td>240</td>
-										</tr>
-									</tbody>
-								</table>
+class Movie extends Component{
+
+	constructor(){
+		super()
+        this.state = {
+            movies: "",
+        }
+	}
+
+	componentDidMount(){
+		axios("http://localhost:3001/api/movies")
+		.then(response => {
+			console.log(response.data.movies);
+			this.setState({movies: response.data.movies});
+		})
+		.catch(error=>{
+			console.log(error);
+		})
+	}
+
+	render(){
+
+		return(
+			<>
+						{/*<!-- PRODUCTS LIST -->*/}
+						<h1 className="h3 mb-2 text-gray-800">All the movies in the Database</h1>
+						
+						{/*<!-- DataTales Example -->*/}
+						<div className="card shadow mb-4">
+							<div className="card-body">
+								<div className="table-responsive">
+
+									<table className="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+										<thead>
+											<tr>
+												<th>Id</th>
+												<th>Titulo</th>
+												<th>Calificación</th>
+												<th>Premios</th>
+												<th>Duración</th>
+											</tr>
+										</thead>
+										<tfoot>
+											<tr>
+												<th>Id</th>
+												<th>Titulo</th>
+												<th>Calificación</th>
+												<th>Premios</th>
+												<th>Duración</th>
+											</tr>
+										</tfoot>
+										<tbody>
+											
+										{
+										this.state.movies !== "" ? this.state.movies.map(movie => 
+											<MovieList
+											key={movie.id}
+											id={movie.id}
+											title={movie.title}
+											calificacion={movie.rating}
+											premios={movie.awards}
+											duracion={movie.length}
+											/>
+											)
+											: <h1>Cargando....</h1>
+										}
+
+										</tbody>
+									</table>
+								</div>
 							</div>
-						</div>
-					</div>            
-        </React.Fragment>
-    )
+						</div>            
+			</>
+		)
+	}
+    
 }
 export default Movie;
